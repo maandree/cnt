9dc8ae5a9a771ea58f9818bb8e75a93fc5aa0ac2
Mattias Andrée <maandree@operamail.com>
2012-04-12 22:52:04 +0200

    Adding main class to root package.

---
+++ src/cnt/Program.java
@@ -0,0 +1,40 @@
+/**
+ * Coop Network Tetris — A cooperative tetris over the Internet.
+ * 
+ * Copyright Ⓒ 2012  Mattias Andrée, Peyman Eshtiagh,
+ *                   Calle Lejdbrandt, Magnus Lundberg
+ *
+ * Project for prutt12 (DD2385), KTH.
+ */
+package cnt;
+
+
+/**
+ * This is the main class of the program
+ *
+ * @author  Mattias Andrée, <a href="maandree@kth.se">maandree@kth.se</a>
+ */
+public class Program
+{
+    /**
+     * Non-contructor
+     */
+    private Program()
+    {
+	assert false : "You may not create instances of this class [Program].";
+    }
+    
+    
+    
+    /**
+     * This is the main entry point of the program
+     * 
+     * @param  args  Start up arguments
+     */
+    public static void main(final String... args)
+    {
+	System.out.println("Not implemented");
+    }
+    
+}
+
