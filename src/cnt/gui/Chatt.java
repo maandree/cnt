8684f852dd1991927561a0d13d7fd22ada47bcf1
Mattias Andrée <maandree@operamail.com>
2012-04-16 00:52:51 +0200

    error fix and clean up

--- src/cnt/gui/Chatt.java
+++ src/cnt/gui/Chatt.java
@@ -1,36 +1,43 @@
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+package cnt.gui;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
 
-public class chatt extends JFrame implements ActionListener{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	scrollText ta;
-	chatt() throws IOException{
-		ta = new scrollText();
-		JTextField  da = new JTextField("Skriv h�r");
-	    add(ta);
-	    add(da);
-	    this.setLayout(new GridLayout(2,1));
-		setVisible(true);
-		da.addActionListener(this);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		String temp = ((JTextField) e.getSource()).getText();			//H�mtar texten fr�n omr�det d�r anv�ndaren fyllt i
-		ta.addText(temp, "Magnus", Color.red);
-		((JTextField) e.getSource()).setText("Skriv h�r");		//�terst�ller textomr�det
-	}
-	public static void main(String[] args) throws IOException{
-	    new chatt();
-	}
+/**
+ * @author  Magnus Lundberg
+ */
+public class Chatt extends JFrame implements ActionListener
+{
+    /**
+     * Desired by {@link Serializable}
+     */
+    private static final long serialVersionUID = 1L;
+    
+    scrollText ta;
+    public Chatt() throws IOException
+    {
+	ta = new scrollText();
+	JTextField da = new JTextField("Type message here");
+	add(ta);
+	add(da);
+	this.setLayout(new GridLayout(2,1));
+	setVisible(true);
+	da.addActionListener(this);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	pack();
+    }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+	String temp = ((JTextField) e.getSource()).getText(); //Retrieves typed message
+	ta.addText(temp, "Magnus", Color.red);
+	((JTextField) e.getSource()).setText("Type message here"); //Reset message field
+    }
+    
+    public static void main(String[] args) throws IOException
+    {
+	new chatt();
+    }
 }
