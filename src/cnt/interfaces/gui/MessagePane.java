f2dc183f677c2bf10e00b4622ecc96514117032a
Calle Lejdbrandt <aiqueneldar@gmail.com>
2012-04-17 14:19:40 +0200

    Updated diagram and structure to match correctly. Remeber, diagram is always superir to structure.

---
+++ src/cnt/interfaces/gui/MessagePane.java
@@ -0,0 +1,33 @@
+/**
+ * Coop Network Tetris — A cooperative tetris over the Internet.
+ * 
+ * Copyright Ⓒ 2012  Mattias Andrée, Peyman Eshtiagh,
+ *                   Calle Lejdbrandt, Magnus Lundberg
+ *
+ * Project for prutt12 (DD2385), KTH.
+ */
+package cnt.gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+/**
+ * The message viewer pane in the main frame
+ * 
+ * @author  Magnus Lundberg
+ * @author  Mattias Andrée, <a href="mailto:maandree@kth.se">maandree@kth.se</a>
+ */
+public class MessagePane extends JPanel
+{
+    /**
+     * Constructor
+     */
+    public MessagePane()
+    {
+	this.setLayout(new BorderLayout());
+	this.add(new JScrollPane(new MessageText()), BorderLayout.CENTER);
+    }
+
+}
+
