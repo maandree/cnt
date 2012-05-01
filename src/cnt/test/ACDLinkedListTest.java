6d4ad5ea3d97ae838c9912332413031cc95de2db
Mattias Andrée <maandree@operamail.com>
2012-05-01 22:21:11 +0200

    more verbose and clarified

--- src/cnt/test/ACDLinkedListTest.java
+++ src/cnt/test/ACDLinkedListTest.java
@@ -19,20 +19,39 @@ public class ACDLinkedListTest
     public static void main(final String... args)
     {
 	ACDLinkedList linkedList = new ACDLinkedList();
-	String P= "Peyman";
-	String C= "Calle";
-	String M= "Matte";
-	String F= "Test";
+	
+	String P = "Peyman";
+	String C = "Calle";
+	String M = "Matte";
+	String F = "Test";
+	
+	System.out.println("Adding Peyman");
 	linkedList.insertAfter(P);
+	System.out.println("Adding Calle");
 	linkedList.insertAfter(C);
+	System.out.println("Adding Mattias");
 	linkedList.insertAfter(M);
 	
-	System.out.println(linkedList.contains(P));
-	System.out.println(linkedList.contains(C));
-	System.out.println(linkedList.contains(M));
+	assertion("Testing existance of Peyman", linkedList.contains(P));
+	assertion("Testing existance of Calle", linkedList.contains(C));
+	assertion("Testing existance of Matte", linkedList.contains(M));
+	
+	System.out.println("Searching for and removing Matte");
 	linkedList.remove(linkedList.find("Matte"));
-	System.out.println(linkedList.contains(M));
+	assertion("Testing non-existance of Matte", linkedList.contains(M) == false);
+	
+        System.out.println("Adding another");
 	linkedList.insertBefore(F);
-    }    
+
+        System.out.println("\nPassed all!");
+    }
+
+    public static void assertion(final String description, final boolean passed)
+    {
+        System.out.println(description + ": " + (passed ? "passed" : "failed"));
+        if (passed == false)
+            System.exit(-1);
+    }
+
 }
 
