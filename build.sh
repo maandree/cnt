69603e4ba1d833600d9751de80d1350d8ea5594c
Mattias Andrée <maandree@operamail.com>
2012-05-08 17:28:20 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ mkdir bin 2>/dev/null
 
 cp src/*.png bin/
 
-javac7 -Xlint:all,-serial -cp . -s src -d bin src/cnt/{*,*/*,*/*/*}.java
+#javac7 -Xlint:all,-serial -cp . -s src -d bin src/cnt/{*,*/*,*/*/*}.java
+javac7 -Xlint:all,-serial -cp . -s src -d bin src/cnt/{*,{d,g,m,t,u}*/*,*/*/*,n*/{B,O}*}.java
