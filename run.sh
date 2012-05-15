313f2055df8de723374638dcbdd7e14fb3720deb
Mattias Andrée <maandree@operamail.com>
2012-05-15 04:33:53 +0200

    typo

--- run.sh
+++ run.sh
@@ -1,32 +1,32 @@
 jars=":lib/cling-core-1.0.5.jar:lib/cling-support-1.0.5.jar:lib/teleal-common-1.0.13.jar"
 
 if [[ $# = 0 ]]; then
-    java7 -ea -cp bin$jar cnt.Program
+    java7 -ea -cp bin$jars cnt.Program
 
 elif [[ $1 = "main" ]]; then
-    java7 -ea -cp bin$jar cnt.Program
+    java7 -ea -cp bin$jars cnt.Program
 
 elif [[ $1 = "main-da" ]]; then
-    java7 -da -cp bin$jar cnt.Program
+    java7 -da -cp bin$jars cnt.Program
 
 elif [[ $1 = "engine" ]]; then
-    java7 -ea -cp bin$jar cnt.demo.EngineDemo
+    java7 -ea -cp bin$jars cnt.demo.EngineDemo
 
 elif [[ $1 = "frame" ]]; then
-    java7 -ea -cp bin$jar cnt.demo.MainFrameDemo
+    java7 -ea -cp bin$jars cnt.demo.MainFrameDemo
 
 elif [[ $1 = "network" ]]; then
-    java7 -ea -cp bin$jar cnt.demo.NetworkingDemo
+    java7 -ea -cp bin$jars cnt.demo.NetworkingDemo
 
 elif [[ $1 = "upnp" ]]; then
-    java7 -ea -cp bin$jar cnt.demo.UPnPDemo
+    java7 -ea -cp bin$jars cnt.demo.UPnPDemo
 
 elif [[ $1 = "shape" ]]; then
-    java7 -ea -cp bin$jar cnt.test.ShapeTest
+    java7 -ea -cp bin$jars cnt.test.ShapeTest
 
 elif [[ $1 = "linkedlist" ]]; then
-    java7 -ea -cp bin$jar cnt.test.CDLinkedListTest
-    java7 -ea -cp bin$jar cnt.test.ACDLinkedListTest
+    java7 -ea -cp bin$jars cnt.test.CDLinkedListTest
+    java7 -ea -cp bin$jars cnt.test.ACDLinkedListTest
 
 elif [[ $1 = "--completion--" ]]; then
     _run()
