b8e899e68b17cddf062fd60f5e3b90b09dd33fa4
Mattias Andrée <maandree@operamail.com>
2012-05-21 15:09:32 +0200

    please wait message for diagnostics

--- src/cnt/interaction/desktop/NetworkDialogue.java
+++ src/cnt/interaction/desktop/NetworkDialogue.java
@@ -8,6 +8,8 @@
 package cnt.interaction.desktop;
 import cnt.network.Diagnostics;
 
+import se.kth.maandree.libandree.gui.layout.DockLayout;
+
 import javax.swing.*;
 import javax.swing.tree.*;
 import java.awt.*;
@@ -31,7 +33,7 @@ public class NetworkDialogue extends JDialog
 	super(owner, "Network diagnositics", Dialog.ModalityType.MODELESS);
 	
 	this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-	this.setLocationByPlatform(true);
+	//This.setLocationByPlatform(true);
 	
 	this.pack();
 	this.setSize(new Dimension(400, 300));
@@ -46,49 +48,68 @@ public class NetworkDialogue extends JDialog
      */
     private void buildInterior()
     {
-	this.setLayout(new BorderLayout());
+	this.setLayout(new DockLayout()); //doesn't work with border layout
 	
-	String data = Diagnostics.run();
-	
-	if (data.endsWith("\n"))
-	    data = data.substring(0, data.length());
-	String lines[] = data.split("\n");
-	final DefaultMutableTreeNode[] stack = new DefaultMutableTreeNode[lines.length];
-	int ptr = 0;
-	stack[ptr] = new DefaultMutableTreeNode("Diagnostics", true);
-	int indent = -1;
-	for (final String line : lines)
-	{
-	    int ind = 0;
-	    while (line.charAt(ind) == ' ')
-		ind++;
-	    
-	    final DefaultMutableTreeNode node = new DefaultMutableTreeNode(line.substring(ind), true);
-	    if (ind > indent)
-	    {
-		stack[ptr].add(node);
-		stack[++ptr] = node;
-	    }
-	    else if (ind == indent)
-	    {
-		stack[--ptr].add(node);
-		stack[++ptr] = node;
-	    }
-	    else
-	    {
-		ptr -= indent - ind;
-		indent -= indent - ind;
-		stack[--ptr].add(node);
-		stack[++ptr] = node;
-	    }
-	    indent = ind;
-	}
-	
+	final JLabel waiting = new JLabel("Running diagnostics, stand by…");
 	
 	final JTree tree = new JTree();
-	((DefaultTreeModel)(tree.getModel())).setRoot(stack[0]);
 	final JScrollPane scrolls = new JScrollPane(tree);
-	this.add(scrolls, BorderLayout.CENTER);
+	
+	scrolls.setVisible(false);
+	this.add(waiting, DockLayout.FILL);
+	this.add(scrolls, DockLayout.FILL);
+	
+	final Thread thread = new Thread()
+	        {
+		    /**
+		     * {@inheritDoc}
+		     */
+		    @Override
+		    public void run()
+		    {
+			String data = Diagnostics.run();
+			
+			if (data.endsWith("\n"))
+			    data = data.substring(0, data.length());
+			String lines[] = data.split("\n");
+			final DefaultMutableTreeNode[] stack = new DefaultMutableTreeNode[lines.length];
+			int ptr = 0;
+			stack[ptr] = new DefaultMutableTreeNode("Diagnostics", true);
+			int indent = -1;
+			for (final String line : lines)
+			{
+			    int ind = 0;
+			    while (line.charAt(ind) == ' ')
+				ind++;
+			    
+			    final DefaultMutableTreeNode node = new DefaultMutableTreeNode(line.substring(ind), true);
+			    if (ind > indent)
+			    {
+				stack[ptr].add(node);
+				stack[++ptr] = node;
+			    }
+			    else if (ind == indent)
+			    {
+				stack[--ptr].add(node);
+				stack[++ptr] = node;
+			    }
+			    else
+			    {
+				ptr -= indent - ind;
+				indent -= indent - ind;
+				stack[--ptr].add(node);
+				stack[++ptr] = node;
+			    }
+			    indent = ind;
+			}
+			
+			((DefaultTreeModel)(tree.getModel())).setRoot(stack[0]);
+			waiting.setVisible(false);
+			scrolls.setVisible(true);
+		    }
+	        };
+	thread.setDaemon(true);
+	thread.start();
     }
     
 }
