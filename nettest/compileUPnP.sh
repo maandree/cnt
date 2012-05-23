29b6a9ab88222bccbe0565044f95dbba375e49c3
Mattias Andrée <maandree@operamail.com>
2012-05-23 08:37:18 +0200

    removing duplicate jars from nettest

--- nettest/compileUPnP.sh
+++ nettest/compileUPnP.sh
@@ -1,1 +1,1 @@
-javac -cp .:cling-core-1.0.5.jar:cling-support-1.0.5.jar:teleal-common-1.0.13.jar UPnP.java
+javac -cp .:../lib/cling-core-1.0.5.jar:../lib/cling-support-1.0.5.jar:../lib/teleal-common-1.0.13.jar UPnP.java
--- nettest/runUPnP.sh
+++ nettest/runUPnP.sh
@@ -1,1 +1,1 @@
-java -cp .:..:../cnt/nettest:cling-core-1.0.5.jar:cling-support-1.0.5.jar:teleal-common-1.0.13.jar nettest.UPnP
+java -cp .:../lib/cling-core-1.0.5.jar:../lib/cling-support-1.0.5.jar:../lib/teleal-common-1.0.13.jar nettest.UPnP
